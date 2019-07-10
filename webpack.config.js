78c34052dd32b0477b1ff505375369f7a64d3032
伊丽莎不白 <124304937@qq.com>
2019-07-10 10:39:02 +0800

    build: 构建项目脚手架

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][hash].min.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            title: 'Ticker测试',
+            hash: true
+        })
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8000,
+        inline: true
+    }
+};
